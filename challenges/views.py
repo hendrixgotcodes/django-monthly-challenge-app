0c03522d25dfac66bca754fadf1babfd171b7e27
Samuel O. Asare <asare11samuel@gmail.com>
2023-01-07 00:57:57 +0000

    dynamic routing

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
-def index(request):
-    return HttpResponse("hello world")
+def monthly_challenge(request, month):
+    if(month.lower() == "january"):
+        return HttpResponse("January")
+    elif(month.lower() == "february"):
+        return HttpResponse("february")
+    elif(month.lower() == "march"):
+        return HttpResponse("march")
+    else:
+        return HttpResponseNotFound("This month is not supported")
+        
